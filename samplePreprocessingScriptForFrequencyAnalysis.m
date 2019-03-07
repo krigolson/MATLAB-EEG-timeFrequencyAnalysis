dc7e1245bdf91f06d3fcf24be78c83e2251c081e
Olav Krigolson <krigolson@gmail.com>
2019-03-07 09:01:20 -0600

    Update

--- samplePreprocessingScriptForFrequencyAnalysis.m
+++ samplePreprocessingScriptForFrequencyAnalysis.m
@@ -15,10 +15,10 @@ clc;
 [EEG] = doRereference(EEG,{'TP9','TP10'},EEG.chanlocs);
 
 % filter the data, note the higher top end to get at high gamma range
-[EEG] = doFilter(EEG,0.1,100,60,2,500);
+[EEG] = doFilter(EEG,1,30,60,2,500);
 
 % epoch the data
-[EEG] = doEpochData(EEG,{'S202','S203'},[-1000 1000]);
+[EEG] = doEpochData(EEG,{'S202','S203'},[-200 800]);
 
 % implement a baseline correction
 [EEG] = doBaseline(EEG,[-200,0]);
@@ -37,10 +37,10 @@ clc;
 
 % plot the first 30 Hz of signal for channel Fz for each condition, NB,
 % this is 60 data points given the sampling rate and size of data selected
-dataToPlot = squeeze(FFT.data(27,1:60,:));
+dataToPlot = squeeze(FFT.data(52,1:30,:));
 bar(dataToPlot);
 title('FFT Analysis');
-xticks([1:1:60]);
+xticks([1:1:30]);
 xticklabels(FFT.frequencies);
 xlabel('Frequency (Hz)');
 ylabel('Power (uV^2)');
--- samplePreprocessingScriptForWaveletAnalysis.m
+++ samplePreprocessingScriptForWaveletAnalysis.m
@@ -35,21 +35,43 @@ clc;
 % run the FFT transform on each condition
 [WAV] = doWAV(EEG,{'S202','S203'},[-500 -300],1,30,60,6);
 
-% plot the Wavelets for both conditions side by size for channel 27
-condition1WaveletData = squeeze(WAV.data(27,:,:,1));
-condition2WaveletData = squeeze(WAV.data(27,:,:,2));
-subplot(1,2,1);
+% plot the Wavelets for both conditions side by size for channel 52
+condition1WaveletData = squeeze(WAV.data(52,:,:,1));
+condition2WaveletData = squeeze(WAV.data(52,:,:,2));
+condition1PercentData = squeeze(WAV.percent(52,:,:,1));
+condition2PercentData = squeeze(WAV.percent(52,:,:,2));
+
+subplot(2,2,1);
 imagesc(condition1WaveletData);
-title('Channel Fz: Condition One');
+title('Channel Pz: Condition One');
 xlabel('Time (ms)');
 ylabel('Frequency (Hz)');
 set(gca,'YDir','normal');
 xticklabels = EEG.times(1):100:EEG.times(end);
 xticks = linspace(1,size(condition1WaveletData,2),numel(xticklabels));
 set(gca,'XTick',xticks,'XTickLabel',xticklabels);
-subplot(1,2,2);
+subplot(2,2,2);
 imagesc(condition2WaveletData);
-title('Channel Fz: Condition Two');
+title('Channel Pz: Condition Two');
+xlabel('Time (ms)');
+set(gca,'YDir','normal');
+ylabel('Frequency (Hz)');
+xticklabels = EEG.times(1):100:EEG.times(end);
+xticks = linspace(1,size(condition2WaveletData,2),numel(xticklabels));
+set(gca,'XTick',xticks,'XTickLabel',xticklabels);
+
+subplot(2,2,3);
+imagesc(condition1PercentData);
+title('Channel Pz: Condition One');
+xlabel('Time (ms)');
+ylabel('Frequency (Hz)');
+set(gca,'YDir','normal');
+xticklabels = EEG.times(1):100:EEG.times(end);
+xticks = linspace(1,size(condition1WaveletData,2),numel(xticklabels));
+set(gca,'XTick',xticks,'XTickLabel',xticklabels);
+subplot(2,2,4);
+imagesc(condition2PercentData);
+title('Channel Pz: Condition Two');
 xlabel('Time (ms)');
 set(gca,'YDir','normal');
 ylabel('Frequency (Hz)');
